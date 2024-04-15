6a023ae3cd8e59ca327c522998a709fe0434be4f
去年夏天 <admin@tjsky.net>
2024-04-15 08:42:57 +0800

    Update main.py

--- main.py
+++ main.py
@@ -225,7 +225,6 @@ def main():
             client_sign(i, tbs, j["id"], j["name"])
         logger.info("完成第" + str(n) + "个用户签到")
   #  send_email(favorites)
-  #  不需要发送邮件
     logger.info("所有用户签到结束")
 
 
